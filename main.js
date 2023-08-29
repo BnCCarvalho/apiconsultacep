bb83c6f1c8e77c123c8e0340e9b0298b765c35c1
BnC <99913468+BnCCarvalho@users.noreply.github.com>
2023-08-29 17:37:13 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+async function buscaEndereco(cep) {
+  let mensagemErro = document.getElementById("erro");
+  mensagemErro.innerHTML = "";
+  try {
+    const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+      throw Error("CEP não existente!");
+    }
+    let estado = document.getElementById("estado");
+    let cidade = document.getElementById("cidade");
+    let bairro = document.getElementById("bairro");
+    let logradouro = document.getElementById("endereco");
+
+    estado.value = consultaCEPConvertida.uf;
+    cidade.value = consultaCEPConvertida.localidade;
+    bairro.value = consultaCEPConvertida.bairro;
+    logradouro.value = consultaCEPConvertida.logradouro;
+
+    console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
+  } catch (erro) {
+    mensagemErro.innerHTML = `<p id="erro-msg">CEP inválido. Tente novamente informando no formato 00000000.</p>`;
+  }
+}
+
+const cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
